b48fc7614020716a603c3b21729e7bb2cadb1cee
rsalakhov <r.salahov@spectrumlabs.fi>
2022-06-20 19:01:41 +0300

    debug event

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import '@ergolabs/ui-kit/dist/styles/fonts/fonts.less';
 
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { Liquidity } from './pages/Liquidity/Liquidity';
 
 const App: FC = () => {
-  return <Liquidity />;
+  const [n, setN] = useState('no');
+
+  useEffect(() => {
+    (window as any).Telegram.WebApp.onEvent('viewportChanged', (res: any) =>
+      setN(JSON.stringify(res)),
+    );
+  });
+
+  return (
+    <>
+      {n}
+      <Liquidity />
+    </>
+  );
 };
 
 export default App;
